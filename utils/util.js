806b1d869499368178b5ccdfa97aacf7b615cb27
shudingbo <shudingbo@qq.com>
2020-02-20 19:41:14 +0800

    Date use 24 hour format

--- utils/util.js
+++ utils/util.js
@@ -5,7 +5,7 @@ function formatTime(date) {
 	var n = date.getFullYear(); 
 	var y = date.getMonth() + 1;
 	var r = date.getDate(); 
-	var mytime = date.toLocaleTimeString(); 
+	var mytime = date.toLocaleTimeString("en-US", {hour12: false}); 
 	var mytimes = n+ "-" + y + "-" + r + " " + mytime;
   return mytimes;
 }
